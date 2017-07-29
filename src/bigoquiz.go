322fe62189c8b1500da595edbd882d375b95ca6f
Murray Cumming <murrayc@murrayc.com>
2017-07-29 23:47:31 +0200

    Load some quizzes from XML.
    Just the basic information so far (title and id).

--- src/bigoquiz.go
+++ src/bigoquiz.go
@@ -2,30 +2,79 @@ package bigoquiz
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"path/filepath"
+	"fmt"
 	"net/http"
+	"os"
+	"io/ioutil"
 )
 
 type quiz struct {
-	Id        string `json:"id"`
-	Title     string `json:"title"`
-	IsPrivate bool   `json:"isPrivate"`
+  Id        string `json:"id" xml:"id"`
+  Title     string `json:"title" xml:"title"`
+  IsPrivate bool   `json:"isPrivate" xml:"isPrivate"`
 }
 
 func init() {
 	http.HandleFunc("/api/quiz", rest_quizzes)
 }
 
-func rest_quizzes(w http.ResponseWriter, r *http.Request) {
-	var quizzes [2]quiz
-	testQuiz := &quizzes[0]
-	testQuiz.Id = "test-id"
-	testQuiz.Title = "Test Title"
-	testQuiz.IsPrivate = false
+func loadQuiz(id string) (*quiz, error) {
+	var q quiz
 
-	testQuiz = &quizzes[1]
-	testQuiz.Id = "test-id2"
-	testQuiz.Title = "Test Title 2"
-	testQuiz.IsPrivate = true
+        absFilePath, err := filepath.Abs("quizzes/" + id + ".xml")
+	if err != nil {
+		fmt.Println(err)
+		return &q, err
+	}
+
+	file, err := os.Open(absFilePath)
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return &q, err
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+		return &q, err
+	}
+
+	err = xml.Unmarshal(data, &q)
+	if err != nil {
+		fmt.Println(err)
+		return &q, err
+        }
+
+	q.Id = id
+	return &q, nil
+}
+
+func loadQuizzes() ([]*quiz, error) {
+	quizzes := make([]*quiz, 0)
+
+	quizNames := []string {"algorithms", "bigo"}
+	for _, name := range quizNames {
+	  q, err := loadQuiz(name)
+	  if err != nil {
+		  fmt.Println(err)
+		  return quizzes, err
+	  }
+
+	  quizzes = append(quizzes, q)
+	}
+
+	return quizzes, nil
+}
+
+func rest_quizzes(w http.ResponseWriter, r *http.Request) {
+	quizzes, err := loadQuizzes()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json") // normal header
 	w.WriteHeader(http.StatusOK)
