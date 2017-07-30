598696f7f5ec9267bfc8d05c0e466b3828cc2238
Murray Cumming <murrayc@murrayc.com>
2017-07-30 22:28:12 +0200

    Question: Add SectionId and SubSectionId.
    Though these are not in the XML. They will be a denormalization
    filled by the code.

--- src/quiz/question.go
+++ src/quiz/question.go
@@ -3,4 +3,8 @@ package quiz
 type Question struct {
 	HasIdAndTitle
 	Text Text `json:"text,omitempty" xml:"text"`
+
+	// These are not in the XML.
+	SectionId    Text `json:"sectionId,omitempty"`
+	SubSectionId Text `json:"subSectionId,omitempty"`
 }
