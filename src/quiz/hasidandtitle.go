406287964600183b3f1f76aac38cbb1463dc80c3
Murray Cumming <murrayc@murrayc.com>
2017-07-30 15:14:25 +0200

    api/quiz: Load and serve links.
    For questions, sections, and (soon) sub-sections.

--- src/quiz/hasidandtitle.go
+++ src/quiz/hasidandtitle.go
@@ -3,4 +3,7 @@ package quiz
 type HasIdAndTitle struct {
 	Id    string `json:"id" xml:"id,attr"`
 	Title string `json:"title,omitempty" xml:"title"`
+
+	// A URL.
+	Link string `json:"link,omitempty" xml:"link"`
 }
