66cbcab1733f40758a46a1a6118e409cf30c6fd7
Murray Cumming <murrayc@murrayc.com>
2017-07-30 13:34:12 +0200

    Move code for handling app/quiz into restquiz.go.

--- src/bigoquiz.go
+++ src/bigoquiz.go
@@ -1,84 +1,9 @@
 package bigoquiz
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"path/filepath"
-	"strings"
 )
 
 func init() {
-	http.HandleFunc("/api/quiz", restHandleQuizzes)
-}
-
-func filesWithExtension(dirPath string, ext string) ([]string, error) {
-	result := make([]string, 0)
-
-	files, err := ioutil.ReadDir(dirPath)
-	if err != nil {
-		fmt.Println(err)
-		return result, err
-	}
-
-	dotSuffix := "." + ext
-	suffixLen := len(dotSuffix)
-	for _, f := range files {
-
-		name := f.Name()
-		if strings.HasSuffix(name, dotSuffix) {
-			prefix := name[0 : len(name)-suffixLen]
-			result = append(result, prefix)
-		}
-	}
-
-	return result, nil
-}
-
-func loadQuizzes() ([]*quiz, error) {
-	quizzes := make([]*quiz, 0)
-
-	absFilePath, err := filepath.Abs("quizzes")
-	if err != nil {
-		fmt.Println(err)
-		return quizzes, err
-	}
-
-	quizNames, err := filesWithExtension(absFilePath, "xml")
-	if err != nil {
-		fmt.Println(err)
-		return quizzes, err
-	}
-
-	for _, name := range quizNames {
-		q, err := loadQuiz(name)
-		if err != nil {
-			fmt.Println(err)
-			return quizzes, err
-		}
-
-		quizzes = append(quizzes, q)
-	}
-
-	return quizzes, nil
-}
-
-func restHandleQuizzes(w http.ResponseWriter, r *http.Request) {
-	quizzes, err := loadQuizzes()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Header().Set("Content-Type", "application/json") // normal header
-	w.WriteHeader(http.StatusOK)
-
-	jsonStr, err := json.Marshal(quizzes)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	w.Write(jsonStr)
+	http.HandleFunc("/api/quiz", restHandleQuiz)
 }
---
+++ src/restquiz.go
@@ -0,0 +1,81 @@
+package bigoquiz
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strings"
+)
+
+func filesWithExtension(dirPath string, ext string) ([]string, error) {
+	result := make([]string, 0)
+
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		fmt.Println(err)
+		return result, err
+	}
+
+	dotSuffix := "." + ext
+	suffixLen := len(dotSuffix)
+	for _, f := range files {
+
+		name := f.Name()
+		if strings.HasSuffix(name, dotSuffix) {
+			prefix := name[0 : len(name)-suffixLen]
+			result = append(result, prefix)
+		}
+	}
+
+	return result, nil
+}
+
+func loadQuizzes() ([]*quiz, error) {
+	quizzes := make([]*quiz, 0)
+
+	absFilePath, err := filepath.Abs("quizzes")
+	if err != nil {
+		fmt.Println(err)
+		return quizzes, err
+	}
+
+	quizNames, err := filesWithExtension(absFilePath, "xml")
+	if err != nil {
+		fmt.Println(err)
+		return quizzes, err
+	}
+
+	for _, name := range quizNames {
+		q, err := loadQuiz(name)
+		if err != nil {
+			fmt.Println(err)
+			return quizzes, err
+		}
+
+		quizzes = append(quizzes, q)
+	}
+
+	return quizzes, nil
+}
+
+func restHandleQuiz(w http.ResponseWriter, r *http.Request) {
+        // TODO: Cache this.
+	quizzes, err := loadQuizzes()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json") // normal header
+	w.WriteHeader(http.StatusOK)
+
+	jsonStr, err := json.Marshal(quizzes)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(jsonStr)
+}
