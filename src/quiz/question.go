143f347cc944632ffe08d75ae02ed78ba167697f
Murray Cumming <murrayc@murrayc.com>
2017-07-31 22:39:29 +0200

    QuestionAndAnswer: Split some into Question.

---
+++ src/quiz/question.go
@@ -0,0 +1,10 @@
+package quiz
+
+type Question struct {
+	HasIdAndTitle
+	Text Text `json:"text,omitempty" xml:"text"`
+
+	// These are not in the XML.
+	SectionId    Text `json:"sectionId,omitempty"`
+	SubSectionId Text `json:"subSectionId,omitempty"`
+}
--- src/quiz/questionandanswer.go
+++ src/quiz/questionandanswer.go
@@ -1,11 +1,6 @@
 package quiz
 
 type QuestionAndAnswer struct {
-	HasIdAndTitle
-	Text Text `json:"text,omitempty" xml:"text"`
-	Answer Text `json:"answer,omitempty" xml:"answer"`
-
-	// These are not in the XML.
-	SectionId    Text `json:"sectionId,omitempty"`
-	SubSectionId Text `json:"subSectionId,omitempty"`
+	Question `json:"question,omitempty"` // TODO: Name this field (as Question) and till unmarhal the XML properly.
+	Answer   Text                        `json:"answer,omitempty" xml:"answer"`
 }
