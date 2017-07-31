cc0ce75e3090bc86b36df4ec40d28917c9f90966
Murray Cumming <murrayc@murrayc.com>
2017-07-31 22:12:16 +0200

    QuestionAndAnswer: Add Answer.
    This will be loaded from the XML file and emitted in the JSON output.

--- src/quiz/questionandanswer.go
+++ src/quiz/questionandanswer.go
@@ -3,6 +3,7 @@ package quiz
 type QuestionAndAnswer struct {
 	HasIdAndTitle
 	Text Text `json:"text,omitempty" xml:"text"`
+	Answer Text `json:"answer,omitempty" xml:"answer"`
 
 	// These are not in the XML.
 	SectionId    Text `json:"sectionId,omitempty"`
