e03a76eb455be0df071cadecb998a814e665f5d6
Murray Cumming <murrayc@murrayc.com>
2017-09-03 22:37:22 +0200

    Quiz: setQuestionsChoicesFromAnswers(): Limit number of choices per question.
    Using the same logic as in gwt-bigoquiz.

--- src/quiz/quiz.go
+++ src/quiz/quiz.go
@@ -8,6 +8,8 @@ import (
 	"os"
 )
 
+const maxChoicesFromAnswers = 6
+
 type Quiz struct {
 	HasIdAndTitle
 	IsPrivate bool `json:"isPrivate" xml:"isPrivate"`
@@ -63,26 +65,26 @@ func (self *Quiz) buildQuestionsMapAndArray() {
 	for _, q := range self.Questions {
 		self.questionsMap[q.Id] = q
 
-		self.questionsArray = append(self.questionsArray, q);
+		self.questionsArray = append(self.questionsArray, q)
 	}
 
 	for _, s := range self.Sections {
 		for _, q := range s.Questions {
-		    self.addQuestionToMapAndArray(q, s, nil)
+			self.addQuestionToMapAndArray(q, s, nil)
 		}
 
-		if (s.AnswersAsChoices) {
-		    setQuestionsChoicesFromAnswers(s.Questions)
+		if s.AnswersAsChoices {
+			setQuestionsChoicesFromAnswers(s.Questions)
 		}
 
 		for _, sub := range s.SubSections {
-		    for _, q := range sub.Questions {
-		        self.addQuestionToMapAndArray(q, s, sub)
-		    }
+			for _, q := range sub.Questions {
+				self.addQuestionToMapAndArray(q, s, sub)
+			}
 
-		    if (sub.AnswersAsChoices) {
-		      setQuestionsChoicesFromAnswers(sub.Questions)
-		    }
+			if sub.AnswersAsChoices {
+				setQuestionsChoicesFromAnswers(sub.Questions)
+			}
 		}
 	}
 
@@ -91,54 +93,106 @@ func (self *Quiz) buildQuestionsMapAndArray() {
 /** Make sure that questions have their section ID, sub-section IDs, and choices.
  */
 func (self *Quiz) setQuestionDetails(qa *QuestionAndAnswer, section *Section, subSection *SubSection) {
-  if (qa == nil) {
-    return;
-  }
+	if qa == nil {
+		return
+	}
 
-  q := &(qa.Question);
+	q := &(qa.Question)
 
-  // Update the section and subSection,
-  // so we can return it in the JSON,
-  // so the caller of the REST API does not need to discover these details separately.
-  if (section != nil) {
-      q.SectionId = section.Id
-      q.Section = &(section.HasIdAndTitle)
-  }
+	// Update the section and subSection,
+	// so we can return it in the JSON,
+	// so the caller of the REST API does not need to discover these details separately.
+	if section != nil {
+		q.SectionId = section.Id
+		q.Section = &(section.HasIdAndTitle)
+	}
 
-  if (subSection != nil) {
-      q.SubSectionId = subSection.Id
-      q.SubSection = &(subSection.HasIdAndTitle)
-  }
+	if subSection != nil {
+		q.SubSectionId = subSection.Id
+		q.SubSection = &(subSection.HasIdAndTitle)
+	}
 }
 
 /** Add to the map and array.
  * And also make sure that questions have their section ID and sub-section IDs.
  */
 func (self *Quiz) addQuestionToMapAndArray(qa *QuestionAndAnswer, section *Section, subSection *SubSection) {
-  self.setQuestionDetails(qa, section, subSection)
+	self.setQuestionDetails(qa, section, subSection)
 
-  self.questionsMap[qa.Id] = qa
-  self.questionsArray = append(self.questionsArray, qa);
+	self.questionsMap[qa.Id] = qa
+	self.questionsArray = append(self.questionsArray, qa)
 }
 
 func setQuestionsChoicesFromAnswers(questions []*QuestionAndAnswer) {
-        // Build the list of answers, avoiding duplicates:
-        answers := make([]*Text, 0, len(questions))
+	// Build the list of answers, avoiding duplicates:
+	choices := make([]*Text, 0, len(questions))
 	used := make(map[string]bool)
 
 	for _, q := range questions {
 		t := q.Answer.Text
-		_, ok := used[t];
+		_, ok := used[t]
 		if ok {
-		  continue;
+			continue
 		}
 
-		used[t] = true;
-		answers = append(answers, &(q.Answer));
+		used[t] = true
+		choices = append(choices, &(q.Answer))
 	}
 
+	tooManyChoices := len(choices) > maxChoicesFromAnswers
+
 	for _, q := range questions {
-	        q.Choices = answers
+		if !tooManyChoices {
+			q.Choices = choices
+		} else {
+			reduced := reduceChoices(choices, &(q.Answer))
+			q.Choices = reduced
+		}
+	}
+}
+
+/**
+ * Create a small-enough set of choices which
+ * always contains the correct answer.
+ * This is slow.
+ */
+func reduceChoices(choices []*Text, answer *Text) []*Text {
+
+	result := make([]*Text, len(choices))
+	copy(result, choices)
+	shuffle(result)
+
+	answerIndex, ok := getIndexInArray(choices, answer)
+	if !ok {
+		return nil
+	}
+
+	if answerIndex >= maxChoicesFromAnswers {
+		result = choices[0 : maxChoicesFromAnswers-1]
+		result = append(result, answer)
+		shuffle(result)
+	} else {
+		result = choices[0:maxChoicesFromAnswers]
+	}
+
+	return result
+}
+
+func getIndexInArray(array []*Text, str *Text) (int, bool) {
+	for i, s := range array {
+		if s == str {
+			return i, true
+		}
+	}
+
+	return -1, false
+}
+
+func shuffle(array []*Text) {
+
+	for i := len(array) - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		array[i], array[j] = array[j], array[i]
 	}
 }
 
@@ -158,5 +212,5 @@ func (self *Quiz) GetRandomQuestion() *Question {
 	count := len(self.questionsArray)
 	i := rand.Intn(count - 1)
 	var qa *QuestionAndAnswer = self.questionsArray[i]
-	return &(qa.Question);
+	return &(qa.Question)
 }
