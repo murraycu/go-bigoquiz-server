52b8ad898f6dc0a24e3c0d0d49000cde0db984f1
Murray Cumming <murrayc@murrayc.com>
2017-09-08 23:09:50 +0200

    Quiz: Add a simple test case.

---
+++ src/quiz/quiz_test.go
@@ -0,0 +1,40 @@
+package quiz
+
+import (
+	"testing"
+	"path/filepath"
+)
+
+func TestLoadQuizWithBadFilepath(t *testing.T) {
+	id := "doesnotexist"
+	absFilePath, err := filepath.Abs("../bigoquiz/quizzes/" + id + ".xml")
+	if (err != nil) {
+		t.Error("Could not find file.", err)
+	}
+
+	q, err := LoadQuiz(absFilePath, id)
+	if (err == nil) {
+		t.Error("LoadQuiz() did not faile with an error.")
+	}
+
+	if (q != nil) {
+		t.Error("LoadQuiz() returned a quiz when it should have returned nil.")
+	}
+}
+
+func TestLoadQuiz(t *testing.T) {
+	id := "bigo"
+	absFilePath, err := filepath.Abs("../bigoquiz/quizzes/" + id + ".xml")
+	if (err != nil) {
+		t.Error("Could not find file.", err)
+	}
+
+	q, err := LoadQuiz(absFilePath, id)
+	if (err != nil) {
+		t.Error("LoadQuiz() failed.", err)
+	}
+
+	if (q.Sections == nil) {
+		t.Error("The quiz has no sections.")
+	}
+}
