ac8be1537e91692c75bc2c9b2837fb9dd5b43e5f
Murray Cumming <murrayc@murrayc.com>
2017-09-08 15:51:26 +0200

    user.Profile: Specify lowercase datastore field names.
    So it is more like the gwt-bigoquiz UserProfile entity, maybe
    letting us reuse that data.

--- src/user/profile.go
+++ src/user/profile.go
@@ -3,12 +3,11 @@ package user
 import "golang.org/x/oauth2"
 
 type Profile struct {
-	Id string
-	Name string
-	Email string
+	Name string `datastore:"name"`
+	Email string `datastore:"email"`
 
 	// Google's "sub" ID. See https://developers.google.com/identity/protocols/OpenIDConnect#obtainuserinfo
-	GoogleId string
+	GoogleId string `datastore:"googleId"`
 
-	GoogleAccessToken oauth2.Token
+	GoogleAccessToken oauth2.Token `datastore:"googleAccessToken"`
 }
