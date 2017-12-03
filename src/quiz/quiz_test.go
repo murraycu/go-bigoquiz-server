2387e637b54b7e8d09d3c64516bae1712dc736a4
Murray Cumming <murrayc@murrayc.com>
2017-12-03 21:52:45 +0100

    Test: Quiz: Test GetSection() and getQuestionAndAnswer().

--- src/quiz/quiz_test.go
+++ src/quiz/quiz_test.go
@@ -37,4 +37,61 @@ func TestLoadQuiz(t *testing.T) {
 	if q.Sections == nil {
 		t.Error("The quiz has no sections.")
 	}
+
+	const SECTION_ID = "data-structure-operations"
+	section := q.GetSection(SECTION_ID)
+	if section == nil {
+		t.Error("The quiz does not have the expected section.")
+	}
+
+	if section.Id != SECTION_ID {
+		t.Error("The section does not have the expected ID.")
+	}
+
+	if section.Title != "Data Structure Operations" {
+		t.Error("The section does not have the expected title.")
+	}
+
+	if section.Link != "" {
+		t.Error("The section does not have the expected link.")
+	}
+
+	const QUESTION_ID = "b-tree-search-worst"
+	qa := q.GetQuestionAndAnswer(QUESTION_ID)
+	if qa == nil {
+		t.Error("The quiz does not have the expected question.")
+	}
+
+	if qa.Question.Id != QUESTION_ID {
+		t.Error("The question does not have the expected ID.")
+	}
+
+	if qa.Question.SectionId != SECTION_ID {
+		t.Error("The question does not have the expected SectionId.")
+	}
+
+	if qa.Question.Section.Id != SECTION_ID {
+		t.Error("The question does not have the expected section ID.")
+	}
+
+	if qa.Question.Section.Title != "Data Structure Operations" {
+		t.Error("The question does not have the expected section ID.")
+	}
+
+	const SUB_SECTION_ID = "b-tree"
+	if qa.Question.SubSectionId != SUB_SECTION_ID {
+		t.Error("The question does not have the expected sub-section ID.")
+	}
+
+	if qa.Question.SubSection.Id != SUB_SECTION_ID {
+		t.Error("The question does not have the expected sub-section ID.")
+	}
+
+	if qa.Question.SubSection.Title != "B-Tree" {
+		t.Error("The question does not have the expected sub-section Title.")
+	}
+
+	if len(qa.Question.Choices) == 0 {
+		t.Error("The question does not have any choices")
+	}
 }
