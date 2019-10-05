3712fd97048b7e0a15e8453b019c41078c3d07c7
Murray Cumming <murrayc@murrayc.com>
2019-10-05 21:45:20 +0200

    config: Generate*OAuthConfig(): Use only secrets from config files
    The other details, such as the Endpoint (AuthURL and TokenURL) are not
    secret. And they are now set based on the overall base URL, so we don't
    need to have separate oauth2 credentials files for different
    environments.

--- config/config.go
+++ config/config.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/facebook"
+	"golang.org/x/oauth2/github"
 	"golang.org/x/oauth2/google"
 	"io/ioutil"
 	"path/filepath"
@@ -13,6 +15,13 @@ const (
 	BaseUrl = "https://bigoquiz.com"
 	// When running angular-bigoquiz-client with ng serve: BaseUrl = "http://localhost:4200"
 
+	BaseApiUrl = "https://api.bigoquiz.com"
+	// When running locally: BaseApuUrl = "http://localhost:8080
+
+	PART_URL_LOGIN_CALLBACK_GOOGLE   = "callback-google"
+	PART_URL_LOGIN_CALLBACK_GITHUB   = "callback-github"
+	PART_URL_LOGIN_CALLBACK_FACEBOOK = "callback-facebook"
+
 	// This file contains other secrets, such as the keys for the encrypted cookie store.
 	// The file format is like so:
 	//
@@ -21,10 +30,9 @@ const (
 	// }
 	configFilename = "config.json"
 
-	// This file must be downloaded
-	// (via the "DOWNLOAD JSON" link at https://console.developers.google.com/apis/credentials/oauthclient )
-	// and added with this exact filename, next to the main.go source file.
-	googleConfigCredentialsFilename = "google_credentials_secret.json"
+	googleConfigCredentialsFilename   = "google_credentials_secret.json"
+	githubConfigCredentialsFilename   = "github_credentials_secret.json"
+	facebookConfigCredentialsFilename = "facebook_credentials_secret.json"
 
 	// See https://developers.google.com/+/web/api/rest/oauth#profile
 	googleCredentialsScopeProfile = "profile"
@@ -34,21 +42,12 @@ const (
 
 	// This has the same format, and location, as googleConfigCredentialsFilename,
 	// but is maintained manually instead of being downloaded.
-	// See https://github.com/settings/applications/SOME_ID_OF_YOUR_OWN_APP
-	// and https://github.com/golang/oauth2
+
 	// for clues
-	githubConfigCredentialsFilename = "github_credentials_secret.json"
 
 	// See https://developer.github.com/apps/building-integrations/setting-up-and-registering-oauth-apps/about-scopes-for-oauth-apps/
 	githubCredentialsScopeUser  = "read:user"
 	githubCredentialsScopeEmail = "user:email"
-
-	// This has the same format, and location, as googleConfigCredentialsFilename,
-	// but is maintained manually instead of being downloaded.
-	// See https://developers.facebook.com/apps/YOUR_OWN_APPS_CLIENT_ID/settings/basic/
-	// and https://github.com/golang/oauth2/blob/master/facebook/facebook.go
-	// for clues.
-	facebookConfigCredentialsFilename = "facebook_credentials_secret.json"
 
 	// See https://developers.facebook.com/docs/facebook-login/permissions
 	facebookCredentialsScopePublicProfile = "public_profile"
@@ -79,41 +78,97 @@ func GenerateConfig() (*Config, error) {
 }
 
 /** Get an oauth2 Config object based on the secret .json file,
- * in the Google format expected by google.ConfigFromJSON(),
- * though this is also for non-Google credentials.
+ * These files contains the client_id and client_secret for the OAuth2 authentication.
+ * See github_credentials_secret.json.example, for instance.
+ *
+ * Google:
+ * (See the "DOWNLOAD JSON" link at https://console.developers.google.com/apis/credentials/oauthclient )
+ * and added with this exact filename, next to the main.go source file.
+ *
+ * Github:
+ * See https://github.com/settings/applications/SOME_ID_OF_YOUR_OWN_APP
+ * and https://github.com/golang/oauth2
+ *
+ * Facebook:
+ * See https://developers.facebook.com/apps/YOUR_OWN_APPS_CLIENT_ID/settings/basic/
+ * and https://github.com/golang/oauth2/blob/master/facebook/facebook.go
+ * for clues.
  */
-func generateOAuthConfig(credentialsFilename string, scope ...string) (*oauth2.Config, error) {
+func addSecretsToOAuthConfig(credentialsFilenamePrefix string, config oauth2.Config) (*oauth2.Config, error) {
+	credentialsFilename := fmt.Sprintf("%s_credentials_secret.json", credentialsFilenamePrefix)
 	path := filepath.Join("config_oauth2", credentialsFilename)
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		return nil, fmt.Errorf("unable to read client secret file (%s): %v", credentialsFilename, err)
 	}
 
-	config, err := google.ConfigFromJSON(b, scope...)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse client secret file (%s) to config: %v", credentialsFilename, err)
+	// Load the secrets:
+	type cred struct {
+		ClientID     string `json:"client_id"`
+		ClientSecret string `json:"client_secret"`
 	}
 
-	return config, nil
+	var secretConfig cred
+	if err := json.Unmarshal(b, &secretConfig); err != nil {
+		return nil, fmt.Errorf("Unable to parse client secret file (%s) to config (json.Unmarshal() failed): %v", credentialsFilename, err)
+	}
+
+	config.ClientID = secretConfig.ClientID
+	config.ClientSecret = secretConfig.ClientSecret
+
+	return &config, nil
 }
 
 /** Get an oauth2 Config object based on the secret .json file.
  * See googleConfigCredentialsFilename.
  */
 func GenerateGoogleOAuthConfig() (*oauth2.Config, error) {
-	return generateOAuthConfig(googleConfigCredentialsFilename, googleCredentialsScopeProfile, googleCredentialsScopeEmail)
+	config := oauth2.Config{
+		ClientID:     "", // Filled in from secrets
+		ClientSecret: "", // Filled in from secrets
+		Endpoint:     google.Endpoint,
+		RedirectURL:  callbackUrl(PART_URL_LOGIN_CALLBACK_GOOGLE),
+		Scopes:       []string{googleCredentialsScopeProfile, googleCredentialsScopeEmail},
+	}
+
+	result, err := addSecretsToOAuthConfig("google", config)
+	if err != nil {
+		return nil, fmt.Errorf("addSecretsToOAuthConfig() failed: %v", err)
+	}
+
+	return result, nil
+}
+
+func callbackUrl(suffix string) string {
+	return BaseApiUrl + "/login/" + suffix
 }
 
 /** Get an oauth2 Config object based on the secret .json file.
  * See githubConfigCredentialsFilename.
  */
 func GenerateGitHubOAuthConfig() (*oauth2.Config, error) {
-	return generateOAuthConfig(githubConfigCredentialsFilename, githubCredentialsScopeUser, githubCredentialsScopeEmail)
+	config := oauth2.Config{
+		ClientID:     "", // Filled in from secrets
+		ClientSecret: "", // Filled in from secrets
+		Endpoint:     github.Endpoint,
+		RedirectURL:  callbackUrl(PART_URL_LOGIN_CALLBACK_GITHUB),
+		Scopes:       []string{githubCredentialsScopeUser, githubCredentialsScopeEmail},
+	}
+
+	return addSecretsToOAuthConfig("github", config)
 }
 
 /** Get an oauth2 Config object based on the secret .json file.
  * See githubConfigCredentialsFilename.
  */
 func GenerateFacebookOAuthConfig() (*oauth2.Config, error) {
-	return generateOAuthConfig(facebookConfigCredentialsFilename, facebookCredentialsScopePublicProfile, facebookCredentialsScopeEmail)
+	config := oauth2.Config{
+		ClientID:     "", // Filled in from secrets
+		ClientSecret: "", // Filled in from secrets
+		Endpoint:     facebook.Endpoint,
+		RedirectURL:  callbackUrl(PART_URL_LOGIN_CALLBACK_FACEBOOK),
+		Scopes:       []string{facebookCredentialsScopePublicProfile, facebookCredentialsScopeEmail},
+	}
+
+	return addSecretsToOAuthConfig("facebook", config)
 }
