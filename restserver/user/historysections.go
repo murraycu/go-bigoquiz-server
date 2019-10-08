27c48268cd705129fb7be91b6619cfdca702d735
Murray Cumming <murrayc@murrayc.com>
2019-10-08 12:45:21 +0200

    Move some of domain/user to restserver/user
    Becaue these data structures are only used in the REST/JSON API.

--- restserver/user/historysections.go
+++ restserver/user/historysections.go
@@ -1,9 +1,13 @@
 package user
 
+import (
+	"github.com/murraycu/go-bigoquiz-server/domain/user"
+)
+
 type HistorySections struct {
 	LoginInfo LoginInfo `json:"loginInfo,omitempty"`
 	QuizId    string    `json:"quizId,omitempty"`
 	QuizTitle string    `json:"quizTitle,omitempty"`
 
-	Stats []*Stats `json:"stats,omitempty"`
+	Stats []*user.Stats `json:"stats,omitempty"`
 }
