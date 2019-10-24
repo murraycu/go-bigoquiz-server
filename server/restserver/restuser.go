92ccca9afe866713f53ac01e53b090e354fb297e
Murray Cumming <murrayc@murrayc.com>
2019-10-24 10:43:16 +0200

    restserver: getProfileFromSessionAndDb(): Don't return token
    The caller doesn't really need it.

--- server/restserver/restuser.go
+++ server/restserver/restuser.go
@@ -6,7 +6,6 @@ import (
 	domainuser "github.com/murraycu/go-bigoquiz-server/domain/user"
 	"github.com/murraycu/go-bigoquiz-server/repositories/db"
 	restuser "github.com/murraycu/go-bigoquiz-server/server/restserver/user"
-	"golang.org/x/oauth2"
 	"net/http"
 )
 
@@ -21,54 +20,51 @@ func (s *RestServer) HandleUser(w http.ResponseWriter, r *http.Request, ps httpr
 }
 
 // Returns the LoginInfo and the userID.
-func (s *RestServer) getProfileFromSessionAndDb(r *http.Request) (*domainuser.Profile, string, *oauth2.Token, error) {
+func (s *RestServer) getProfileFromSessionAndDb(r *http.Request) (*domainuser.Profile, string, error) {
 	userId, token, err := s.userSessionStore.GetProfileFromSession(r)
 	if err != nil {
-		return nil, "", nil, fmt.Errorf("GetProfileFromSession() failed: %v", err)
+		return nil, "", fmt.Errorf("GetProfileFromSession() failed: %v", err)
 	}
 
 	if len(userId) == 0 {
 		// Not an error.
 		// It's just not in the session cookie.
-		return nil, "", nil, nil
+		return nil, "", nil
 	}
 
 	dbClient, err := db.NewUserDataRepository()
 	if err != nil {
-		return nil, "", nil, fmt.Errorf("NewUserDataRepository() failed: %v", err)
+		return nil, "", fmt.Errorf("NewUserDataRepository() failed: %v", err)
 	}
 
 	c := r.Context()
 	profile, err := dbClient.GetUserProfileById(c, userId)
 	if err != nil {
-		return nil, "", nil, fmt.Errorf("GetUserProfileById() failed: %v", err)
+		return nil, "", fmt.Errorf("GetUserProfileById() failed: %v", err)
 	}
 
-	return profile, userId, token, nil
+	return profile, userId, nil
 }
 
 // Returns the LoginInfo and the userID.
 func (s *RestServer) getLoginInfoFromSessionAndDb(r *http.Request) (*restuser.LoginInfo, string, error) {
 	var loginInfo restuser.LoginInfo
 
-	profile, userId, token, err := s.getProfileFromSessionAndDb(r)
+	profile, userId, err := s.getProfileFromSessionAndDb(r)
 	if err != nil {
 		loginInfo.LoggedIn = false
 		loginInfo.ErrorMessage = fmt.Sprintf("not logged in (%v)", err)
 	}
 
-	s.updateLoginInfoFromProfile(&loginInfo, profile, token)
+	s.updateLoginInfoFromProfile(&loginInfo, profile)
 
 	return &loginInfo, userId, err
 }
 
-func (s *RestServer) updateLoginInfoFromProfile(loginInfo *restuser.LoginInfo, profile *domainuser.Profile, token *oauth2.Token) {
+func (s *RestServer) updateLoginInfoFromProfile(loginInfo *restuser.LoginInfo, profile *domainuser.Profile) {
 	if profile == nil {
 		loginInfo.LoggedIn = false
 		loginInfo.ErrorMessage = "not logged in user (no profile found)"
-	} else if !token.Valid() {
-		loginInfo.LoggedIn = false
-		loginInfo.ErrorMessage = "not logged in user (invalid token)"
 	} else {
 		loginInfo.LoggedIn = true
 		loginInfo.Nickname = profile.Name
