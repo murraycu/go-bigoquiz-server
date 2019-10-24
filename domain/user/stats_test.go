cd5003e79b799b8638666285427d64abf1f80805
Murray Cumming <murrayc@murrayc.com>
2019-10-24 22:11:06 +0200

    stats_test: Test more methods

--- domain/user/stats_test.go
+++ domain/user/stats_test.go
@@ -21,3 +21,94 @@ func TestStatsCountAnsweredWrong(t *testing.T) {
 	stats.UpdateProblemQuestion(TEST_QUESTION_ID, true)
 	assert.Equal(t, 1, stats.GetQuestionCountAnsweredWrong(TEST_QUESTION_ID))
 }
+
+func TestStatsGetQuestionWasAnsweredWithWrong(t *testing.T) {
+	var stats Stats
+
+	assert.False(t, stats.GetQuestionWasAnswered(TEST_QUESTION_ID))
+
+	stats.UpdateProblemQuestion(TEST_QUESTION_ID, false)
+	assert.True(t, stats.GetQuestionWasAnswered(TEST_QUESTION_ID))
+}
+
+func TestStatsGetQuestionWasAnsweredWithCorrect(t *testing.T) {
+	var stats Stats
+
+	assert.False(t, stats.GetQuestionWasAnswered(TEST_QUESTION_ID))
+
+	stats.UpdateProblemQuestion(TEST_QUESTION_ID, true)
+	assert.True(t, stats.GetQuestionWasAnswered(TEST_QUESTION_ID))
+}
+
+func TestStatsGetQuestionCountAnsweredWrong(t *testing.T) {
+	var stats Stats
+
+	assert.Equal(t, 0, stats.GetQuestionCountAnsweredWrong(TEST_QUESTION_ID))
+
+	stats.UpdateProblemQuestion(TEST_QUESTION_ID, true)
+	assert.Equal(t, -1, stats.GetQuestionCountAnsweredWrong(TEST_QUESTION_ID))
+
+	stats.UpdateProblemQuestion(TEST_QUESTION_ID, false)
+	assert.Equal(t, 0, stats.GetQuestionCountAnsweredWrong(TEST_QUESTION_ID))
+}
+
+func TestStatsIncrementAnswered(t *testing.T) {
+	var stats Stats
+
+	assert.Equal(t, 0, stats.Answered)
+
+	stats.IncrementAnswered()
+	assert.Equal(t, 1, stats.Answered)
+
+	stats.IncrementAnswered()
+	assert.Equal(t, 2, stats.Answered)
+}
+
+func TestStatsIncrementCorrect(t *testing.T) {
+	var stats Stats
+
+	assert.Equal(t, 0, stats.Correct)
+
+	stats.IncrementCorrect()
+	assert.Equal(t, 1, stats.Correct)
+
+	stats.IncrementCorrect()
+	assert.Equal(t, 2, stats.Correct)
+}
+
+func TestStatsIncrementAnsweredOnce(t *testing.T) {
+	var stats Stats
+
+	assert.Equal(t, 0, stats.CountQuestionsAnsweredOnce)
+
+	stats.UpdateProblemQuestion("some-question-1", true)
+	assert.Equal(t, 1, stats.CountQuestionsAnsweredOnce)
+
+	stats.UpdateProblemQuestion("some-question-2", true)
+	assert.Equal(t, 2, stats.CountQuestionsAnsweredOnce)
+
+	stats.UpdateProblemQuestion("some-question-3", false)
+	assert.Equal(t, 3, stats.CountQuestionsAnsweredOnce)
+}
+
+func TestStatsIncrementAnsweredCorrectOnce(t *testing.T) {
+	var stats Stats
+
+	assert.Equal(t, 0, stats.CountQuestionsCorrectOnce)
+
+	// This should only increment for correct answers, and only for questions that have not been correct before.
+	stats.UpdateProblemQuestion("some-question-1", true)
+	assert.Equal(t, 1, stats.CountQuestionsCorrectOnce)
+
+	stats.UpdateProblemQuestion("some-question-2", true)
+	assert.Equal(t, 2, stats.CountQuestionsCorrectOnce)
+
+	stats.UpdateProblemQuestion("some-question-2", true)
+	assert.Equal(t, 2, stats.CountQuestionsCorrectOnce)
+
+	stats.UpdateProblemQuestion("some-question-3", false)
+	assert.Equal(t, 2, stats.CountQuestionsCorrectOnce)
+
+	stats.UpdateProblemQuestion("some-question-4", true)
+	assert.Equal(t, 3, stats.CountQuestionsCorrectOnce)
+}
