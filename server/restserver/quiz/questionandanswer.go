f34e0258d9b9e7899ced2a461af8e368ba053813
Murray Cumming <murrayc@murrayc.com>
2019-10-26 12:57:58 +0200

    restserver: Remove unused QuestionAndAnswer.createReverse()

--- server/restserver/quiz/questionandanswer.go
+++ server/restserver/quiz/questionandanswer.go
@@ -4,12 +4,3 @@ type QuestionAndAnswer struct {
 	Question `json:"question,omitempty"` // TODO: Name this field (as Question) and xtill unmarhal the XML properly.
 	Answer   Text                        `json:"answer,omitempty"`
 }
-
-func (self *QuestionAndAnswer) createReverse() *QuestionAndAnswer {
-	var result QuestionAndAnswer
-	result.Id = "reverse-" + self.Id
-	result.Text = self.Answer
-	result.Answer = self.Text
-	result.Answer.IsHtml = false
-	return &result
-}
