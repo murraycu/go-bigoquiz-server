6d3264a02d13d27ee95e5ea66bd0226425c6b68e
Murray Cumming <murrayc@murrayc.com>
2025-12-27 12:10:04 +0100

    Revert "quizzesrepository: Add test for XML/JSON loading equivalency"
    This reverts commit 2d09b04a95c9c294917084e0d4ed3abd94697c21.
    
    Because we will not need this when we only load from JSON. This commit
    has served its purpose of verifying the equivalency.

--- repositories/quizzes/quizzesrepository_test.go
+++ repositories/quizzes/quizzesrepository_test.go
@@ -10,7 +10,7 @@ import (
 
 // TODO: Add tests that don't use the real quizzes.
 
-func loadRealQuizzes(t *testing.T, asJson bool, addReverses bool) MapQuizzes {
+func loadRealQuizzes(t *testing.T) MapQuizzes {
 	directoryFilepath, err := filepath.Abs("../../quizzes")
 	assert.Nil(t, err)
 	assert.NotNil(t, directoryFilepath)
@@ -19,7 +19,7 @@ func loadRealQuizzes(t *testing.T, asJson bool, addReverses bool) MapQuizzes {
 	assert.Nil(t, err)
 	assert.NotNil(t, quizzesStore)
 
-	quizzes, err := quizzesStore.LoadQuizzes( /*asJson*/ false /*addReverses*/, true)
+	quizzes, err := quizzesStore.LoadQuizzes()
 	assert.Nil(t, err)
 	assert.NotNil(t, quizzes)
 
@@ -27,44 +27,40 @@ func loadRealQuizzes(t *testing.T, asJson bool, addReverses bool) MapQuizzes {
 }
 
 func TestQuizzesRepositoryNewWithRealQuizzes(t *testing.T) {
-	for _, asJson := range []bool{true, false} {
-		restQuizzes := loadRealQuizzes(t, asJson /*addReverses=*/, true)
+	restQuizzes := loadRealQuizzes(t)
 
-		for quizId, quiz := range restQuizzes {
-			assert.NotNil(t, quizId)
-			assert.NotNil(t, quiz)
-		}
+	for quizId, quiz := range restQuizzes {
+		assert.NotNil(t, quizId)
+		assert.NotNil(t, quiz)
 	}
 }
 
 func TestQuizzesRepositoryRealQuizHasQuestions(t *testing.T) {
-	for _, asJson := range []bool{true, false} {
-		restQuizzes := loadRealQuizzes(t, asJson /*addReverses=*/, true)
+	restQuizzes := loadRealQuizzes(t)
 
-		for _, quiz := range restQuizzes {
-			assert.NotNil(t, quiz)
+	for _, quiz := range restQuizzes {
+		assert.NotNil(t, quiz)
 
-			questionsFound := false
-			if len(quiz.Questions) > 0 {
-				questionsFound = true
-			} else {
-				for _, section := range quiz.Sections {
-					if len(section.Questions) > 0 {
+		questionsFound := false
+		if len(quiz.Questions) > 0 {
+			questionsFound = true
+		} else {
+			for _, section := range quiz.Sections {
+				if len(section.Questions) > 0 {
+					questionsFound = true
+					break
+				}
+
+				for _, subSection := range section.SubSections {
+					if len(subSection.Questions) > 0 {
 						questionsFound = true
 						break
 					}
-
-					for _, subSection := range section.SubSections {
-						if len(subSection.Questions) > 0 {
-							questionsFound = true
-							break
-						}
-					}
 				}
 			}
-
-			assert.True(t, questionsFound)
 		}
+
+		assert.True(t, questionsFound)
 	}
 }
 
@@ -73,25 +69,23 @@ func questionHasAnswer(question *domainquiz.QuestionAndAnswer) bool {
 }
 
 func TestQuizzesRepositoryRealQuizHasAnswers(t *testing.T) {
-	for _, asJson := range []bool{true, false} {
-		restQuizzes := loadRealQuizzes(t, asJson /*addReverses=*/, true)
+	restQuizzes := loadRealQuizzes(t)
 
-		for _, quiz := range restQuizzes {
-			assert.NotNil(t, quiz)
+	for _, quiz := range restQuizzes {
+		assert.NotNil(t, quiz)
 
-			for _, question := range quiz.Questions {
+		for _, question := range quiz.Questions {
+			assert.True(t, questionHasAnswer(question))
+		}
+
+		for _, section := range quiz.Sections {
+			for _, question := range section.Questions {
 				assert.True(t, questionHasAnswer(question))
 			}
 
-			for _, section := range quiz.Sections {
-				for _, question := range section.Questions {
+			for _, subSection := range section.SubSections {
+				for _, question := range subSection.Questions {
 					assert.True(t, questionHasAnswer(question))
-				}
-
-				for _, subSection := range section.SubSections {
-					for _, question := range subSection.Questions {
-						assert.True(t, questionHasAnswer(question))
-					}
 				}
 			}
 		}
@@ -99,44 +93,22 @@ func TestQuizzesRepositoryRealQuizHasAnswers(t *testing.T) {
 }
 
 func TestQuizzesRepositoryRealQuizHasSections(t *testing.T) {
-	for _, asJson := range []bool{true, false} {
-		restQuizzes := loadRealQuizzes(t, asJson /*addReverses=*/, true)
+	restQuizzes := loadRealQuizzes(t)
 
-		for _, quiz := range restQuizzes {
-			assert.NotNil(t, quiz)
+	for _, quiz := range restQuizzes {
+		assert.NotNil(t, quiz)
 
-			assert.NotEmpty(t, quiz.Sections)
-		}
+		assert.NotEmpty(t, quiz.Sections)
 	}
+
 }
 
 func TestQuizzesRepositoryRealQuizHasTitle(t *testing.T) {
-	for _, asJson := range []bool{true, false} {
-		restQuizzes := loadRealQuizzes(t, asJson /*addReverses=*/, true)
+	restQuizzes := loadRealQuizzes(t)
 
-		for _, quiz := range restQuizzes {
-			assert.NotNil(t, quiz)
+	for _, quiz := range restQuizzes {
+		assert.NotNil(t, quiz)
 
-			assert.NotEmpty(t, quiz.Title)
-		}
+		assert.NotEmpty(t, quiz.Title)
 	}
-}
-
-func TestQuizzesRepositoryOneLoadedAsXmlIsEqualToLoadedAsJson(t *testing.T) {
-	fromXml := loadRealQuizzes(t /*asJson=*/, false /*addReverses=*/, true)
-	quizFromXml := fromXml["algorithms_analysis"]
-	assert.NotNil(t, quizFromXml)
-
-	fromJson := loadRealQuizzes(t /*asJson=*/, true /*addReverses=*/, true)
-	quizFromJson := fromJson["algorithms_analysis"]
-	assert.NotNil(t, quizFromJson)
-
-	assert.Equal(t, quizFromXml, quizFromJson)
-}
-
-func TestQuizzesRepositoryAllLoadedAsXmlIsEqualToLoadedAsJson(t *testing.T) {
-	fromXml := loadRealQuizzes(t /*asJson=*/, false /*addReverses=*/, true)
-	asJson := loadRealQuizzes(t /*asJson=*/, true /*addReverses=*/, true)
-
-	assert.Equal(t, fromXml, asJson)
 }
