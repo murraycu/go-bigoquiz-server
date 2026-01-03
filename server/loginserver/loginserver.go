4cea7c6a636c88a3e91b232903137cbce296d991
Murray Cumming <murrayc@murrayc.com>
2026-01-03 18:29:17 +0100

    LoginServer: Extract redirectTo*Login() methods
    Just to make the code slightly clearer.

--- server/loginserver/loginserver.go
+++ server/loginserver/loginserver.go
@@ -80,6 +80,10 @@ func (s *LoginServer) generateOAuthUrl(r *http.Request, oauthConfig *oauth2.Conf
 }
 
 func (s *LoginServer) HandleGoogleLogin(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	s.redirectToGoogleLogin(w, r)
+}
+
+func (s *LoginServer) redirectToGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	// Redirect the user to the Google login page:
 	url, err := s.generateOAuthUrl(r, s.confOAuthGoogle)
 	if err != nil {
@@ -303,6 +307,10 @@ func (s *LoginServer) generateGitHubOAuthUrl(r *http.Request) string {
 }
 
 func (s *LoginServer) HandleGitHubLogin(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	s.redirectToGitHubLogin(w, r)
+}
+
+func (s *LoginServer) redirectToGitHubLogin(w http.ResponseWriter, r *http.Request) {
 	// Redirect the user to the GitHub login page:
 	url, err := s.generateOAuthUrl(r, s.confOAuthGitHub)
 	if err != nil {
@@ -314,6 +322,10 @@ func (s *LoginServer) HandleGitHubLogin(w http.ResponseWriter, r *http.Request, 
 }
 
 func (s *LoginServer) HandleFacebookLogin(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	s.redirectToFacebookLogin(w, r)
+}
+
+func (s *LoginServer) redirectToFacebookLogin(w http.ResponseWriter, r *http.Request) {
 	// Redirect the user to the Facebook login page:
 	url, err := s.generateOAuthUrl(r, s.confOAuthFacebook)
 	if err != nil {
