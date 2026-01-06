0bf8191b38123921e6dcf192dd39af9f3f2ca4b2
Murray Cumming <murrayc@murrayc.com>
2026-01-06 15:18:29 +0100

    OAuthClient: Request, and store, OAuth refresh token too
    So we also receive an OAuth refresh token (longer lived), not just an OAuth
    access token (short-lived - approximately 30 minutes). We will use the refresh
    token to retrieve a new access token. This is appropriate because this is a
    "Web Server Application" (also known as a "Server-side Web App"):
    https://developers.google.com/identity/protocols/oauth2/web-server so it's
    considered safe for us to store the (longer lived) refresh token on the server.
    
    In contrast, a "Client-side Web Applications" would not be a safe place to
    expose the refresh token. So it would instead redirect the web page to the
    Google/GitHub/Facebook login page again, which would, most of the time, just
    quickly redirect back again without any user interaction.

--- repositories/db/dtos/user/profile.go
+++ repositories/db/dtos/user/profile.go
@@ -7,17 +7,23 @@ type Profile struct {
 	Email string `datastore:"email"`
 
 	// Google's "sub" ID. See https://developers.google.com/identity/protocols/OpenIDConnect#obtainuserinfo
-	GoogleId          string       `datastore:"googleId"`
+	GoogleId string `datastore:"googleId"`
+
+	// GoogleAccessToken is actually an oauth2.Token, not an access token, but contains an access token (and a refresh token).
 	GoogleAccessToken oauth2.Token `datastore:"googleAccessToken"`
 	GoogleProfileUrl  string       `datastore:"googleProfileUrl"`
 
 	// GitHub's ID. See https://developer.github.com/v3/users/#get-a-single-user
-	GitHubId          int          `datastore:"gitHubId"`
+	GitHubId int `datastore:"gitHubId"`
+
+	// GitHubAccessToken is actually an oauth2.Token, not an access token, but contains an access token (and a refresh token).
 	GitHubAccessToken oauth2.Token `datastore:"gitHubAccessToken"`
 	GitHubProfileUrl  string       `datastore:"gitHubProfileUrl"`
 
 	// Facebook's ID. See TODO
-	FacebookId          string       `datastore:"facebookId"`
+	FacebookId string `datastore:"facebookId"`
+
+	// FacebookAccessToken is actually an oauth2.Token, not an access token, but contains an access token (and a refresh token).
 	FacebookAccessToken oauth2.Token `datastore:"facebookAccessToken"`
 	FacebookProfileUrl  string       `datastore:"facebookProfileUrl"`
 }
